5e1189763ae3c812db6ced2e20c56202f2dde7e8
makhles <mr.lange85@gmail.com>
2016-09-17 15:27:58 -0300

    created menu bar with Exit option

--- src/view/Gomoku.java
+++ src/view/Gomoku.java
@@ -1,8 +1,14 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Gomoku extends JFrame {
 
@@ -13,10 +19,29 @@ public class Gomoku extends JFrame {
     }
 
     private void initUI() {
+        createMenuBar();
         setTitle("Gomoku");
         setSize(600, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        fileMenu.add(exitMenuItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
     }
 
     public static void main(String args[]) {
