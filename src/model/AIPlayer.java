696e6603358463a0f82d97ce277241f32929f1f5
makhles <mr.lange85@gmail.com>
2016-09-20 20:22:38 -0300

    minmax-alphabet initial implementation

--- src/model/AIPlayer.java
+++ src/model/AIPlayer.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 public class AIPlayer implements Player {
 
+    private final int DEPTH = 10;
     private StoneType stoneType;
     private Random randomGenerator;
 
@@ -18,6 +19,8 @@ public class AIPlayer implements Player {
     @Override
     public void play(BoardConfig config, PlayInfo info) {
         // TODO
+//        applyMinMaxAlphaBeta(config, DEPTH, -Integer.MAX_VALUE, Integer.MAX_VALUE, true);
+
         Tile tile = new Tile(7, 7, stoneType);
         if (!config.getEmptyNeighbours().isEmpty()) {
             List<Tile> emptyNeighbours = new ArrayList<>(config.getEmptyNeighbours());
@@ -37,6 +40,35 @@ public class AIPlayer implements Player {
         } else {
             System.out.println("AIPlayer::play(): Tile is null.");
         }
+    }
+
+    private int applyMinMaxAlphaBeta(BoardConfig config, int depth, int alpha, int beta, boolean max) {
+        BoardConfig child;
+        int value, tmp = 0;
+        if (depth == 0) return evaluateHeuristic(config);
+        if (max) {
+            value = - Integer.MAX_VALUE;
+            while ((child = config.generateConfig(StoneType.WHITE)) != null) {
+                tmp = applyMinMaxAlphaBeta(child, depth - 1, alpha, beta, false);
+                if (tmp > value) value = tmp;
+                if (value > alpha) alpha = value;
+                if (beta <= alpha) break;  // Beta pruning
+            }
+        } else {
+            value = Integer.MAX_VALUE;
+            while ((child = config.generateConfig(StoneType.WHITE)) != null) {
+                tmp = applyMinMaxAlphaBeta(child, depth - 1, alpha, beta, true);
+                if (tmp < value) value = tmp;
+                if (value < beta) beta = value;
+                if (beta <= alpha) break;  // Alpha pruning
+            }
+        }
+        return value;
+    }
+
+    private int evaluateHeuristic(BoardConfig config) {
+        // TODO Auto-generated method stub
+        return 0;
     }
 
     @Override
